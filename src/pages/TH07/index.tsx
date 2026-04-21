37fc97c72216ef54a26cd8de9dd950230357281d
Vu <vudm.ptit@gmailgmailcom>
2026-04-21 09:44:30 +0700

    TH07

--- src/pages/TH07/index.tsx
+++ src/pages/TH07/index.tsx
@@ -1,8 +1,17 @@
+import { Tabs } from 'antd';
 import React from 'react';
 import BlogApp from './BlogApp';
 
 const TH07: React.FC = () => {
-    return <BlogApp />;
+    return (
+        <div style={{ padding: 20 }}>
+            <Tabs defaultActiveKey="1">
+                <Tabs.TabPane tab="Blog Cá Nhân" key="1">
+                    <BlogApp />
+                </Tabs.TabPane>
+            </Tabs>
+        </div>
+    );
 };
 
 export default TH07;
